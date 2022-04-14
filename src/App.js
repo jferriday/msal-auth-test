5cfafaa18545538316352e1cfc84a7caf2ecc4b2
Josh Ferriday <70819699+jferriday@users.noreply.github.com>
2022-04-14 14:42:21 +0100

    finsh config

--- src/App.js
+++ src/App.js
@@ -5,7 +5,25 @@ import "./App.css";
 import { useAuth } from "react-oidc-context";
 
 function App() {
-	const auth = useAuth();
+  const auth = useAuth();
+  
+   async function makeFetchCall(token) {
+			console.log(token);
+			const headers = new Headers();
+			headers.append("Authorization", "Bearer " + token);
+			try {
+				const response = await fetch(
+					"https://auth-setup-ded-express.azurewebsites.net/hello",
+					{
+						headers: headers,
+					}
+				);
+				const responseText = await response.text();
+				return responseText;
+			} catch (e) {
+				console.warn(e);
+			}
+		}
 
 	function handleLogin() {
 		auth.signinPopup();
@@ -15,8 +33,11 @@ function App() {
 		auth.signoutPopup();
 	}
 
-	function getToken() {
-		console.log(auth.user);
+  async function hitApi() {
+    console.log(auth)
+    const token = auth.user.id_token
+    const response = await makeFetchCall(token)
+    console.log(response)
 	}
 
 	return (
@@ -27,7 +48,7 @@ function App() {
 			) : (
 				<button onClick={handleLogin}>Sign In</button>
 			)}
-			<button onClick={getToken}>Log Token</button>
+			<button onClick={hitApi}>Hit API</button>
 		</div>
 	);
 }
